ec3752423bcfda02b62c761bc78ae1591107b72e
yanadekel <75662763+yanadekel@users.noreply.github.com>
2021-04-24 11:50:33 +0300

    stage change

--- ex17.1-product_model/index.js
+++ ex17.1-product_model/index.js
@@ -1,25 +1,29 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
 
 
-const app = express();
-const productsRouter = require('./routes/products.route');
+// const app = express();
+// const productsRouter = require('./routes/products.route');
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
 
-app.use('/api/products', productsRouter); 
+// app.use('/api/products', productsRouter); 
 
-mongoose.connect('mongodb://localhost/productscollection', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-}).then(() => {
-    console.log("database connect")
-});
+// mongoose.connect("mongodb+srv://YANA:HPm9ccWcwm98VkV @cluster0.h0he6.mongodb.net/test", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true
+// }).then(() => {
+//     console.log("database connect")
+// });
 
-app.listen(process.env.PORT || 5002, () => {
-    console.log(`application start at ${process.env.PORT || 5002}`)
-})
+// app.listen(process.env.PORT || 5002, () => {
+//     console.log(`application start at ${process.env.PORT || 5002}`)
+// })
+
+
+
+
